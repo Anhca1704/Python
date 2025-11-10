6c1102245a89456f74e403805241649cfd3dd66e
Anhca1704 <anhca1704@gmail.com>
2025-11-10 23:38:04 +0700

    Revert "Add files via upload"
    This reverts commit 5e32a78539ba3dc315383663657213363901a078.

--- B2-Số fibonacci.py
+++
@@ -1,9 +0,0 @@
-def fibo(n):
-	A=[0,1]
-	while A[-1]+A[-2]<=n:
-		A.append(A[-1]+A[-2])
-	return A
-n=int(input())
-B=fibo(10**5)
-for i in range(n-1):
-	print(B[i],end=" ")
--- Số fibonacci.py
+++
@@ -1,7 +0,0 @@
-def fibo(n):
-	A=[0,1]
-	while A[-1]+A[-2]<=n:
-		A.append(A[-1]+A[-2])
-	return A
-n=int(input())
-print(fibo(n))
