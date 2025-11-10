64217e389937931a386f17dc893b0cbdea829fbe
Anhca1704 <anhca1704@gmail.com>
2025-11-10 23:38:06 +0700

    Revert "Add files via upload"
    This reverts commit f620cf3055cc371d73939f0c0684541ecf1600f0.

--- B1-Số Nguyên tố.py
+++
@@ -1,9 +0,0 @@
-import math
-n=int(input())
-A=[True]*(n+1)
-A[1]=A[0]=False
-for i in range (2,int(math.sqrt(n))+1):
-	if A[i]:
-		for j in range(i*i,n+1,i):
-			A[j]=False
-print([i for i in range(n) if A[i]])
